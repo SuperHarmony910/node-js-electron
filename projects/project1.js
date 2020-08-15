5f384a35cb8a599eede2074b17985bf3c712b232
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2020-08-15 22:48:20 +1000

    Vocabulary issuee rectified.

--- projects/project1.js
+++ projects/project1.js
@@ -4,6 +4,6 @@ var dt = require('./myfirstmodule');
 
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write("The date and time are currently: " + dt.myDateTime());
+    res.write("The date and time is currently: " + dt.myDateTime());
     res.end();
 }).listen(128);
