b0ce51cccf3a166732bbba0985396047ac7a4e59
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2020-08-16 20:50:38 +1000

    Added the PowerShell run file to the commits!

--- projects/project1.js
+++ projects/project1.js
@@ -1,9 +1,10 @@
-/* A tutorial I'm taking from w3schools!*/
+/* A tutorial I'm taking from w3schools! */
 var http = require('http');
-var dt = require('./myfirstmodule');
+var dt = require('./project1-module');
 
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
+    console.log("Server is running on localhost:128");
     res.write("The date and time is currently: " + dt.myDateTime());
     res.end();
 }).listen(128);
