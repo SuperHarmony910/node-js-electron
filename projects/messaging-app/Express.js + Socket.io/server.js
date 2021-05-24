6b29d2027ad092b8e72527b91dcba84b8ae9a511
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2021-05-25 08:40:22 +1000

    Could we resurrect the project with React?

--- projects/messaging-app/Express.js + Socket.io/server.js
+++ projects/messaging-app/Express.js + Socket.io/server.js
@@ -1,15 +1,48 @@
+// The libraries
 var express = require('express');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var mongoose = require('mongoose');
 const MongoClient = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
 var dbUrl = 'mongodb+srv://dbUser:messaging-app@clusterofdoom.t73v5.mongodb.net/messaging-app?retryWrites=true&w=majority'
 
+// Database connection
 mongoose.connect(dbUrl, (err) => {
     console.log('MongoDB successfully connected!', err);
     })
 
+    var Message = mongoose.model('Message', { name : String, message : String})
+
+    // Client connection alert
+    io.on('connection', () =>{
+        console.log('A user has connected')
+       })
+
+    // Body parser
+
+
+
 var app = express();
 
-var server = app.listen(5000, () => {
+// HTTP GET request
+app.get('/messages', (req, res) => {
+    Message.find({},(err, messages)=> {
+      res.send(messages);
+    })
+  })
+
+  // HTTP POST request
+  app.post('/messages', (req, res) => {
+    var message = new Message(req.body);
+    message.save((err) =>{
+      if(err)
+        sendStatus(500);
+      res.sendStatus(200);
+    })
+  })
+
+var server = app.listen(3000, () => {
     console.log('Server is running on port', server.address().port);
 });
 
